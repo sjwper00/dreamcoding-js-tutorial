37a2ae618de8771dde1c0fbb4e0bbc006dd48c4f
sjwper00 <jinwons94@naver.com>
2022-01-11 22:13:38 +0900

    jinwon 6th commit

--- async/promise.js
+++ async/promise.js
@@ -10,12 +10,31 @@ const promise = new Promise((resolve, reject) => {
     //doing some heavy work(ex network, read files, 이렇게 시간이 오래 걸리는 작업들이 완료될 때까지 다음 작업이 대기되어야하기 때문에 promise로 비동기적으로 처리하는게 좋다
     console.log('doing something...');
     setTimeout(()=>{
-        //sresolve('ellie');
-        reject(new Error('no network'))
+        //resolve('ellie');
+        reject(new Error('no network'))//new Error()는 error가 발생했다는 걸 나타내는 object
     }, 2000);
-})//object 생성
+});//object 생성
 
 // 2. Consumers: then, catch, finally
-promise.then((value)=>{ // 최종적으로 resolve라는 callback함수를 통해서 전달한 값이 value의 parameter 전달된 것을 알 수 있다.
+promise.then(value=>{ // 최종적으로 resolve라는 callback함수를 통해서 전달한 값이 value의 parameter 전달된 것을 알 수 있다.
     console.log(value);
 })
+.catch(error => {   //catch()를 통해 error가 나타났을 때 handling하는 callback함수를 등록한다
+    console.log(error);
+})
+.finally(()=>{console.log('finally')});//resolve든 reject든 어떤 기능을 마지막으로 수행하고 싶을 때는 finally를 사용한다
+
+//3. Promise chaining
+const fetchNumber = new Promise((resolve, reject)=>{
+    setTimeout(()=> resolve(1), 1000);
+})
+
+fetchNumber 
+.then(num => num * 2)
+.then(num => num * 3)
+.then(num => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(num-1), 1000);
+    });
+})
+.then(num => console.log(num));
